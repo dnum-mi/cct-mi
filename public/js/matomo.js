d2c85e64579953337167acf6d1fb8e7e6d851257
mogador26 <hassan.driss@9online.fr>
2023-12-16 21:32:15 +0100

    modify dockerfile, add ignore file package-lock.json; add link of a specific website matomo (scw)

--- public/js/matomo.js
+++ public/js/matomo.js
@@ -4,9 +4,9 @@ _paq.push(["setExcludedQueryParams", ["simulationId", "_csrf"]]);
 _paq.push(['trackPageView']);
 _paq.push(['enableLinkTracking']);
 (function () {
-    var u = "https://stats.data.gouv.fr/";
+    var u = "https://matomo-dtnum-mi.freedynamicdns.org/";
     _paq.push(['setTrackerUrl', u + 'matomo.php']);
-    _paq.push(['setSiteId', '95']);
+    _paq.push(['setSiteId', '2']);
     var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
     g.async = true;
     g.src = u + 'matomo.js';
